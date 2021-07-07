3dc77510e79bd71821930d11c9dc92ad41d30376
Khanhaseebahmad <86571421+Khanhaseebahmad@users.noreply.github.com>
2021-07-07 19:39:32 +0530

    First Java Programs on Github
    This is my first GitHub Upload..

---
+++ knightingale/firstmaths.java
@@ -0,0 +1,15 @@
+package knightingale;
+
+public class firstmaths {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=200;
+		int b=100;
+		// sum operator
+		int sum=a-b;
+		
+		System.out.println("the sum is ..."+sum);
+	}
+
+}
