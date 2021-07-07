21e3fc771f58eda63f47709f0ecdaee926877773
Khanhaseebahmad <86571421+Khanhaseebahmad@users.noreply.github.com>
2021-07-07 21:45:58 +0530

    Add files via upload

---
+++ BooleanOperations.java
@@ -0,0 +1,11 @@
+package knightingale;
+
+public class BooleanOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(20==20);
+	}
+
+}
